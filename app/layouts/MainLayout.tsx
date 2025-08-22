1f08a85b085a8ae64b92f3431243d81a4d32beb3
Shrikant <shrikantbk06@gmail.com>
2025-08-22 12:52:16 +1000

    Added SideNavigation features

--- app/layouts/MainLayout.tsx
+++ app/layouts/MainLayout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { usePathname } from "next/navigation";
 import TopNav from "./includes/TopNav";
+import SideNavMain from "./includes/SideNavMain";
 
 export default function MainLayout({
   children,
@@ -13,7 +14,8 @@ export default function MainLayout({
     <>
         <TopNav />
         <div className={`flex justify-between mx-auto w-full lg:px-2.5 px-0 ${pathname == '/' ? 'max-w-[1140px]' : ''}`}>
-            {children}
+          <SideNavMain />
+          {children}
         </div>
     </>
   );
